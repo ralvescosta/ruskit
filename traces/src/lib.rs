07876f2de4a6b9ea0e26fa03a73139d66003dde7
ralvescsota <rafael.rac.mg@gmail.com>
2022-10-23 17:04:49 -0300

    feat: traces crate

---
+++ traces/src/lib.rs
@@ -0,0 +1,57 @@
+///deprecated
+pub mod amqp;
+
+pub mod jaeger;
+pub mod otlp;
+
+use env::{Config, Environment};
+use opentelemetry::trace::TraceContextExt;
+use opentelemetry::{
+    global::BoxedTracer,
+    sdk::trace::Sampler,
+    trace::{SpanKind, Tracer},
+    Context,
+};
+use std::borrow::Cow;
+
+fn get_sampler(cfg: &Config) -> Sampler {
+    if cfg.env == Environment::Local {
+        return Sampler::AlwaysOn;
+    }
+
+    let sampler = Sampler::TraceIdRatioBased(0.8);
+    return Sampler::ParentBased(Box::new(sampler));
+}
+
+pub fn span_ctx(tracer: &BoxedTracer, kind: SpanKind, name: &str) -> Context {
+    let span = tracer
+        .span_builder(Cow::from(name.to_owned()))
+        .with_kind(kind)
+        .start(tracer);
+
+    Context::current_with_span(span)
+}
+
+pub fn trace_id(ctx: &Context) -> String {
+    let span = ctx.span();
+
+    if span.is_recording() {
+        let span_ctx = span.span_context();
+
+        return span_ctx.trace_id().to_string();
+    }
+
+    String::new()
+}
+
+pub fn span_id(ctx: &Context) -> String {
+    let span = ctx.span();
+
+    if span.is_recording() {
+        let span_ctx = span.span_context();
+
+        return span_ctx.span_id().to_string();
+    }
+
+    String::new()
+}

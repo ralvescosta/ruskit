78b818ab6b71eaceb91ef753f13425ae3891ced7
ralvescosta <rafael.rac.mg@gmail.com>
2022-12-19 14:08:05 -0300

    feat: postgres

--- postgres/src/lib.rs
+++ postgres/src/lib.rs
@@ -5,11 +5,11 @@ use tokio_postgres::NoTls;
 
 pub fn conn_pool(cfg: &env::Config) -> Result<Pool, Box<dyn Error>> {
     let mut pg_cfg = tokio_postgres::Config::new();
-    pg_cfg.host(&cfg.db_host);
-    pg_cfg.port(cfg.db_port);
-    pg_cfg.dbname(&cfg.db_name);
-    pg_cfg.user(&cfg.db_user);
-    pg_cfg.password(&cfg.db_password);
+    pg_cfg.host(&cfg.postgres.host);
+    pg_cfg.port(cfg.postgres.port);
+    pg_cfg.dbname(&cfg.postgres.db);
+    pg_cfg.user(&cfg.postgres.user);
+    pg_cfg.password(&cfg.postgres.password);
 
     let mgr_cfg = ManagerConfig {
         recycling_method: RecyclingMethod::Fast,

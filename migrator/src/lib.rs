96e0ccde0f31357a8a432426a3532c90d04f09a2
ralvescosta <rafael.rac.mg@gmail.com>
2023-02-01 06:35:02 -0300

    feat: sqlite migrator

--- migrator/src/lib.rs
+++ migrator/src/lib.rs
@@ -1,11 +1,11 @@
 #[cfg(feature = "postgres")]
 mod postgres;
 mod service;
-// #[cfg(feature = "sqlite")]
+#[cfg(feature = "sqlite")]
 mod sqlite;
 
 #[cfg(feature = "postgres")]
 pub use postgres::PostgresDriver;
 pub use service::Migrator;
-// #[cfg(feature = "sqlite")]
+#[cfg(feature = "sqlite")]
 pub use sqlite::SqliteDriver;

19067db037ef2a61af2bf47c7e860044100dbdcf
ralvescosta <rafael.rac.mg@gmail.com>
2023-03-23 17:18:23 -0300

    feat: dispatcher

--- amqp/src/dispatcher.rs
+++ amqp/src/dispatcher.rs
@@ -3,7 +3,7 @@ use async_trait::async_trait;
 use futures_util::{future::join_all, StreamExt};
 use lapin::{options::BasicConsumeOptions, types::FieldTable, Channel};
 use opentelemetry::{global, Context};
-use std::{collections::HashMap, fmt::Debug, sync::Arc, vec};
+use std::{collections::HashMap, fmt::Display, sync::Arc, vec};
 use tokio::task::JoinError;
 use tracing::error;
 
@@ -22,73 +22,52 @@ pub struct DispatcherDefinition {
 pub trait Dispatcher<'d> {
     fn register<T>(
         self,
-        queue: &str,
-        msg: T,
+        def: &'d QueueDefinition,
+        msg: &'d T,
         handler: Arc<dyn ConsumerHandler + Send + Sync>,
     ) -> Self
     where
-        T: Debug;
-    fn queue_definition(self, def: &'d QueueDefinition) -> Self;
-    fn queues_definition(self, queues_def: HashMap<&'d str, &'d QueueDefinition>) -> Self;
+        T: Display;
 }
 
-pub struct AmqpDispatcher<'ad> {
+pub struct AmqpDispatcher {
     channel: Arc<Channel>,
-    pub(crate) queues_def: HashMap<&'ad str, &'ad QueueDefinition>,
     pub(crate) dispatchers_def: HashMap<String, DispatcherDefinition>,
 }
 
-impl<'ad> AmqpDispatcher<'ad> {
-    pub fn new(channel: Arc<Channel>) -> AmqpDispatcher<'ad> {
+impl AmqpDispatcher {
+    pub fn new(channel: Arc<Channel>) -> AmqpDispatcher {
         return AmqpDispatcher {
             channel,
-            queues_def: HashMap::default(),
             dispatchers_def: HashMap::default(),
         };
     }
 }
 
-impl<'ad> Dispatcher<'ad> for AmqpDispatcher<'ad> {
+impl<'ad> Dispatcher<'ad> for AmqpDispatcher {
     fn register<T>(
         mut self,
-        queue: &str,
-        msg: T,
+        def: &'ad QueueDefinition,
+        msg: &'ad T,
         handler: Arc<dyn ConsumerHandler + Send + Sync>,
     ) -> Self
     where
-        T: Debug,
+        T: Display,
     {
-        let queue_def = match self.queues_def.get(queue) {
-            Some(d) => d.to_owned().to_owned(),
-            _ => {
-                panic!("cannot get queue definition")
-            }
-        };
-
         self.dispatchers_def.insert(
-            format!("{:?}", msg),
+            format!("{}", msg),
             DispatcherDefinition {
-                queue: queue.to_owned(),
-                queue_def,
+                queue: def.name.to_owned(),
+                queue_def: def.to_owned(),
                 handler,
             },
         );
 
         self
     }
-
-    fn queue_definition(mut self, def: &'ad QueueDefinition) -> Self {
-        self.queues_def.insert(def.name, def);
-        self
-    }
-
-    fn queues_definition(mut self, queues_def: HashMap<&'ad str, &'ad QueueDefinition>) -> Self {
-        self.queues_def = queues_def;
-        self
-    }
 }
 
-impl<'ad> AmqpDispatcher<'ad> {
+impl AmqpDispatcher {
     pub async fn consume_blocking(&self) -> Vec<Result<(), JoinError>> {
         let mut spawns = vec![];
 

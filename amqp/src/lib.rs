3382b09efe7fe1ac22dc40c2b5e0e182afee4ff2
ralvescosta <rafael.rac.mg@gmail.com>
2023-03-24 08:52:23 -0300

    refactor: remove mock mod

--- amqp/src/lib.rs
+++ amqp/src/lib.rs
@@ -4,10 +4,6 @@ pub mod channel;
 pub mod dispatcher;
 pub mod errors;
 pub mod exchange;
-#[cfg(test)]
-pub mod mocks;
-#[cfg(feature = "mocks")]
-pub mod mocks;
 pub mod otel;
 pub mod publisher;
 pub mod queue;

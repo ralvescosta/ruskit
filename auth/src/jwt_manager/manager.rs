3dd66b1388f3e4b32d25eece1a73dedc665f9fa0
ralvescosta <rafael.rac.mg@gmail.com>
2023-09-17 15:22:34 -0300

    feat: auth0

--- auth/src/jwt_manager/manager.rs
+++ auth/src/jwt_manager/manager.rs
@@ -3,6 +3,14 @@ use opentelemetry::Context;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Default, Serialize, Deserialize)]
+pub struct Session {
+    pub user_id: i64,
+    pub user_uuid: String,
+    pub company_id: i64,
+    pub company_uuid: String,
+}
+
+#[derive(Debug, Default, Serialize, Deserialize)]
 pub struct TokenClaims {
     pub iss: String,
     pub sub: String,
@@ -10,6 +18,7 @@ pub struct TokenClaims {
     pub iat: u64,
     pub exp: u64,
     pub scope: String,
+    pub session: Session,
 }
 
 #[async_trait]

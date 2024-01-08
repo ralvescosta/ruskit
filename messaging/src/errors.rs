29964a0bc77d8640740e246634fea6ab306a6048
ralvescosta <rafael.rac.mg@gmail.com>
2024-01-08 16:41:58 -0300

    feat: messaging crate

--- messaging/src/errors.rs
+++ messaging/src/errors.rs
@@ -14,9 +14,18 @@ pub enum MessagingError {
     #[error("failure to create the consumer")]
     CreatingConsumerError,
 
+    #[error("serializing error")]
+    SerializingError,
+
+    #[error("deserializing error")]
+    DeserializingError,
+
+    #[error("error to handle message")]
+    HandlerError,
+
     #[error("failure to consume message `{0}`")]
     ConsumerError(String),
 
     #[error("failure to publish message")]
-    PublishingError,
+    PublisherError,
 }
--- messaging/src/publisher.rs
+++ messaging/src/publisher.rs
@@ -12,20 +12,62 @@ pub enum HeaderValues {
     LongString(String),
     Int(i8),
     LongInt(i32),
+    LongLongInt(i64),
+    Uint(u8),
+    LongUint(u32),
+    LongLongUint(u64),
+}
+
+impl Into<String> for HeaderValues {
+    fn into(self) -> String {
+        match self {
+            Self::ShortString(v) => v,
+            Self::LongString(v) => v,
+            Self::Int(v) => v.to_string(),
+            Self::LongInt(v) => v.to_string(),
+            Self::LongLongInt(v) => v.to_string(),
+            Self::Uint(v) => v.to_string(),
+            Self::LongUint(v) => v.to_string(),
+            Self::LongLongUint(v) => v.to_string(),
+        }
+    }
 }
 
 #[derive(Clone)]
-pub struct PublishInfos {
-    pub to: String,
+pub struct PublishMessage {
     pub from: String,
+    pub to: String,
     pub key: String,
     pub msg_type: String,
-    pub payload: Box<[u8]>,
+    pub data: Box<[u8]>,
     pub headers: Option<HashMap<String, HeaderValues>>,
+}
+
+impl PublishMessage {
+    pub fn new<T>(
+        from: T,
+        to: T,
+        key: T,
+        msg_type: T,
+        data: &[u8],
+        headers: Option<HashMap<String, HeaderValues>>,
+    ) -> Self
+    where
+        T: Into<String>,
+    {
+        PublishMessage {
+            to: to.into(),
+            from: from.into(),
+            key: key.into(),
+            msg_type: msg_type.into(),
+            data: data.into(),
+            headers,
+        }
+    }
 }
 
 #[cfg_attr(feature = "mocks", automock)]
 #[async_trait]
 pub trait Publisher: Send + Sync {
-    async fn publish(&self, ctx: &Context, infos: &PublishInfos) -> Result<(), MessagingError>;
+    async fn publish(&self, ctx: &Context, msg: &PublishMessage) -> Result<(), MessagingError>;
 }
